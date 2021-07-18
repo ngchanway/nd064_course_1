220769e1a7f275c72b244bf470cb098e0b8b77ae
Ng Chan Way <chanwayng@u.nus.edu>
2021-07-18 09:24:53 +0800

    Update app.py

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,38 @@
 from flask import Flask
+from flask import json
+import logging
+
 app = Flask(__name__)
+
+@app.route('/status')
+def healthcheck():
+    response = app.response_class(
+        json.dumps({'results':'OK - healthy'}),
+        status=200,
+        mimetype='application/json'
+    )
+    # log line
+    app.logger.info('Status request successful')
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+        json.dumps({'status':'success','code':0,'data':{'UserCount':140,'UserCountActive':23}}),
+        status=200,
+        mimetype='application/json'
+    )
+    # log line
+    app.logger.info('Metrics request successful')
+    return response
 
 @app.route("/")
 def hello():
+    # log line
+    app.logger.info('Main request successful')
     return "Hello World!"
 
 if __name__ == "__main__":
+    # stream logs to app.log file
+    logging.basicConfig(filename='app.log',level=logging.DEBUG)
     app.run(host='0.0.0.0')
